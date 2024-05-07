ab1afea75ebfc8ab9f74f96e4cb54e992c14a636
rigo mac <mbandrigo@gmail.com>
2024-05-08 05:58:37 +0800

    perbaikan data diri

--- resources/views/surveior/dashboard.blade.php
+++ resources/views/surveior/dashboard.blade.php
@@ -5,116 +5,58 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
     <title>SI LANNI</title>
-    <meta name="description" content="Aplikasi Tracer Study IAIN Kendari" />
-    <!-- Favicon Tags Start -->
-    <link rel="apple-touch-icon-precomposed" sizes="57x57" href="img/favicon/apple-touch-icon-57x57.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/favicon/apple-touch-icon-114x114.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/favicon/apple-touch-icon-72x72.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/favicon/apple-touch-icon-144x144.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="60x60" href="img/favicon/apple-touch-icon-60x60.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="120x120" href="img/favicon/apple-touch-icon-120x120.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="76x76" href="img/favicon/apple-touch-icon-76x76.png" />
-    <link rel="apple-touch-icon-precomposed" sizes="152x152" href="img/favicon/apple-touch-icon-152x152.png" />
-    <link rel="icon" type="image/png" href="img/favicon/favicon-196x196.png" sizes="196x196" />
-    <link rel="icon" type="image/png" href="img/favicon/favicon-96x96.png" sizes="96x96" />
-    <link rel="icon" type="image/png" href="img/favicon/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="img/favicon/favicon-16x16.png" sizes="16x16" />
-    <link rel="icon" type="image/png" href="img/favicon/favicon-128.png" sizes="128x128" />
-    <meta name="application-name" content="&nbsp;" />
-    <meta name="msapplication-TileColor" content="#FFFFFF" />
-    <meta name="msapplication-TileImage" content="img/favicon/mstile-144x144.png" />
-    <meta name="msapplication-square70x70logo" content="img/favicon/mstile-70x70.png" />
-    <meta name="msapplication-square150x150logo" content="img/favicon/mstile-150x150.png" />
-    <meta name="msapplication-wide310x150logo" content="img/favicon/mstile-310x150.png" />
-    <meta name="msapplication-square310x310logo" content="img/favicon/mstile-310x310.png" />
-    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
-
-    <!-- Favicon Tags End -->
-    <!-- Font Tags Start -->
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600&display=swap" rel="stylesheet" />
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="font/CS-Interface/style.css" />
-    <!-- Font Tags End -->
-
-    <!-- Vendor Styles Start -->
-    <link rel="stylesheet" href="{{asset('/')}}css/vendor/bootstrap.min.css" />
-    <link rel="stylesheet" href="{{asset('/')}}css/vendor/OverlayScrollbars.min.css" />
-    <link rel="stylesheet" href="{{asset('/')}}css/vendor/baguetteBox.min.css" />
-    <!-- Vendor Styles End -->
-    <!-- Template Base Styles Start -->
-    <link rel="stylesheet" href="{{asset('/')}}css/styles.css" />
-    <!-- Template Base Styles End -->
-
-    <link rel="stylesheet" href="{{asset('/')}}css/main.css" />
-    <script src="{{asset('/')}}js/base/loader.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" />
 </head>
 
 <body>
-    <!-- Form Row Start -->
-    <!-- Text Content Start -->
-
-    <main>
-        <div class="container">
-            <section class="scroll-section" id="textContent">
-                <div class="card mb-5">
-                    <div class="card-body d-flex flex-column">
-                        <h3 class="card-title mb-4">Data Tracer</h3>
-                        <ul>
-                            <li>Periode : Tahun 2024</li>
-                        </ul>
-                    </div>
-                </div>
-
-            </section>
-            <!-- Text Content End -->
-            <section class="scroll-section" id="formRow">
-                <!-- <h2 class="small-title">Daftar Pertanyaan</h2> -->
-                <div class="col-auto d-flex mb-2">
-                    <select class="form-select mb-3" id="kabupaten">
-                        <option>Pilih Kabupaten</option>
-                        <option value="KAB. BUTON">KAB. BUTON</option>
-                        <option value="KAB. BUTON SELATAN">KAB. BUTON SELATAN</option>
-                        <option value="KAB. BUTON TENGAH">KAB. BUTON TENGAH</option>
-                        <option value="KAB. BUTON UTARA">KAB. BUTON UTARA</option>
-                        <option value="KAB. KOLAKA">KAB. KOLAKA</option>
-                        <option value="KAB. KOLAKA TIMUR">KAB. KOLAKA TIMUR</option>
-                        <option value="KAB. KOLAKA UTARA">KAB. KOLAKA UTARA</option>
-                        <option value="KAB. KONAWE">KAB. KONAWE</option>
-                        <option value="KAB. KONAWE KEPULAUAN">KAB. KONAWE KEPULAUAN</option>
-                        <option value="KAB. KONAWE SELATAN">KAB. KONAWE SELATAN</option>
-                        <option value="KAB. KONAWE UTARA">KAB. KONAWE UTARA</option>
-                        <option value="KAB. MUNA">KAB. MUNA</option>
-                        <option value="KAB. MUNA BARAT">KAB. MUNA BARAT</option>
-                        <option value="KAB. WAKATOBI">KAB. WAKATOBI</option>
-                        <option value="KOTA BAU-BAU">KOTA BAU-BAU</option>
-                        <option value="KOTA KENDARI">KOTA KENDARI</option>
-                    </select>
-
-                </div>
-                <button onclick="show()" class="btn btn-primary btn-sm">Lihat Progres</button>
-                <div class="card mb-5">
-                    <div class="card-body">
-                        <table class="table table-striped table-hover">
-                            <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">KABUPATEN</th>
-                                    <th scope="col">NIM</th>
-                                    <th scope="col">NAMA</th>
-                                    <th scope="col">Prodi</th>
-                                    <th scope="col">Status</th>
-                                </tr>
-                            </thead>
-                            <tbody id="hasil-show">
-
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            </section>
+    <div class="container mt-5">
+        <div class="card-body">
+            <h3 class="card-title mb-4">Data Tracer Periode Tahun 2024</h3>
         </div>
-    </main>
+        <div class="col-3 d-flex mb-2">
+            <select class="form-select mb-3" id="kabupaten">
+                <option>Pilih Kabupaten</option>
+                <option value="KAB. BUTON">KAB. BUTON</option>
+                <option value="KAB. BUTON SELATAN">KAB. BUTON SELATAN</option>
+                <option value="KAB. BUTON TENGAH">KAB. BUTON TENGAH</option>
+                <option value="KAB. BUTON UTARA">KAB. BUTON UTARA</option>
+                <option value="KAB. KOLAKA">KAB. KOLAKA</option>
+                <option value="KAB. KOLAKA TIMUR">KAB. KOLAKA TIMUR</option>
+                <option value="KAB. KOLAKA UTARA">KAB. KOLAKA UTARA</option>
+                <option value="KAB. KONAWE">KAB. KONAWE</option>
+                <option value="KAB. KONAWE KEPULAUAN">KAB. KONAWE KEPULAUAN</option>
+                <option value="KAB. KONAWE SELATAN">KAB. KONAWE SELATAN</option>
+                <option value="KAB. KONAWE UTARA">KAB. KONAWE UTARA</option>
+                <option value="KAB. MUNA">KAB. MUNA</option>
+                <option value="KAB. MUNA BARAT">KAB. MUNA BARAT</option>
+                <option value="KAB. WAKATOBI">KAB. WAKATOBI</option>
+                <option value="KOTA BAU-BAU">KOTA BAU-BAU</option>
+                <option value="KOTA KENDARI">KOTA KENDARI</option>
+            </select>
+
+        </div>
+        <button onclick="show()" class="btn btn-primary btn-sm mb-3">Lihat Progres</button>
+        <div class="card mb-5">
+            <div class="card-body">
+                <table class="table table-striped table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">KABUPATEN</th>
+                            <th scope="col">NIM</th>
+                            <th scope="col">NAMA</th>
+                            <th scope="col">PRODI</th>
+                            <th scope="col">NO. HP</th>
+                            <th scope="col">STATUS PENGISIAN</th>
+                        </tr>
+                    </thead>
+                    <tbody id="hasil-show">
+
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
     <!-- Form Row End -->
     <div class="modal fade" id="lExample" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-lg">
@@ -138,6 +80,8 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
     <script>
         async function show() {
             let kabupaten = document.querySelector('#kabupaten')
@@ -155,6 +99,7 @@
                 contents += `<td>${data.user.mahasiswa.nim}</td>`
                 contents += `<td>${data.user.mahasiswa.data_diri.nama_lengkap}</td>`
                 contents += `<td>${data.user.mahasiswa.prodi.organisasi_singkatan}</td>`
+                contents += `<td>${data.user.mahasiswa.data_diri.no_hp}</td>`
                 contents += `<td>${(data.sesi_status==0)?"Selesai":"Proses"}</td>`
                 contents += '</tr>'
             })
