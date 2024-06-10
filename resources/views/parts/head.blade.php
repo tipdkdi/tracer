b9b360d157e9546b206f086670937066c5170044
rigo mac <mbandrigo@gmail.com>
2024-06-10 11:53:43 +0800

    https

--- resources/views/parts/head.blade.php
+++ resources/views/parts/head.blade.php
@@ -38,7 +38,7 @@
 <link rel="stylesheet" href="{{asset('/')}}css/vendor/baguetteBox.min.css" />
 <!-- Vendor Styles End -->
 <!-- Template Base Styles Start -->
-<link rel="stylesheet" href="{{asset('/')}}css/styles.css" />
+<!-- <link rel="stylesheet" href="{{asset('/')}}css/styles.css" /> -->
 <!-- Template Base Styles End -->
 
 <link rel="stylesheet" href="{{asset('/')}}css/main.css" />
