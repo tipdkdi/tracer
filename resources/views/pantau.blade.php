d1b44c101084fa293009ce48644baf592fe1ee35
rigo mac <mbandrigo@gmail.com>
2025-09-28 18:22:25 +0800

    2025survei

--- resources/views/pantau.blade.php
+++ resources/views/pantau.blade.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Pemantauan Survei Alumni</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://unpkg.com/vue@3/dist/vue.global.prod.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
